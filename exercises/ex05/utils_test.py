52dd4b1221b0b9c9745951f5779782308dd46a4c
Avery West <aveave@live.unc.edu>
2022-02-22 15:39:17 -0500

    Progress on Exercise 5

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -7,49 +7,57 @@ from exercises.ex05.utils import only_evens, sub, concat
 
 
 def test_only_evens_empty() -> None:
-    """"""
+    """Unit test for edge case for even function definition."""
     numbers: list[int] = []
     assert only_evens(numbers) == []
 
 
 def test_only_evens_odds() -> None:
+    """Unit test for use case for even function definition."""
     numbers: list[int] = [1, 3, 5]
     assert only_evens(numbers) == []
 
 
 def test_only_evens_many_numbers() -> None:
+    """Unit test for use case for even function definition."""
     numbers: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
     assert only_evens(numbers) == [2, 4, 6, 8, 10, 12, 14]
 
 
 def test_sub_empty() -> None:
+    """Unit test for edge case for subset list created."""
     numbers_two: list[int] = []
     assert sub(numbers_two, 1, 3) == []
 
 
 def test_sub_correct() -> None:
+    """Unit test use case for subset list created."""
     numbers_two: list[int] = [10, 20, 30, 40, 50, 60]
     assert sub(numbers_two, 3, 5) == [40, 50]
 
 
 def test_sub_absurd_numbers() -> None:
+    """Unit test use case for subset list created."""
     numbers_two: list[int] = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
     assert sub(numbers_two, -200, 200) == [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
 
 
 def test_concat_empty() -> None:
+    """Unit test edge case for two lists combined."""
     first_list: list[int] = []
     second_list: list[int] = []
     assert concat(first_list, second_list) == []
 
 
 def test_concat_single_items() -> None:
+    """Unit test use case for two lists combined."""
     first_list: list[int] = [5]
     second_list: list[int] = [10]
     assert concat(first_list, second_list) == [5, 10]
 
 
 def test_concat_many_items() -> None:
+    """Unit test use case for two lists combined."""
     first_list: list[int] = [1, 2, 3, 4, 5]
     second_list: list[int] = [6, 7, 8, 9, 10]
     assert concat(first_list, second_list) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
